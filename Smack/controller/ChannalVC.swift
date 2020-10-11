68a8d0cd44955ecbc7b9ef9a47b92b68d633c7bc
abdelrahman-M-abdelfatah <abdoabdo11223399@gmail.com>
2020-10-11 17:47:54 +0200

    4th commit

---
+++ Smack/controller/ChannalVC.swift
@@ -0,0 +1,37 @@
+//
+//  channalVC.swift
+//  Smack
+//
+//  Created by admin on 10/7/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class ChannalVC: UIViewController {
+
+    @IBOutlet weak var LoginBtn: UIButton!
+    override func viewDidLoad() {
+      
+        
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.revealViewController()?.rearViewRevealWidth = self.view.frame.size.width - 60
+    }
+    
+    @IBAction func LoginBtnPressed(_ sender: Any) {
+        performSegue(withIdentifier: "Login", sender: nil   )
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Smack/controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by admin on 10/11/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+    
+    @IBAction func CloseBtn(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
